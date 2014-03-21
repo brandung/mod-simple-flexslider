3168b70a0980f866aad4092c0396d2066e9ca14d
Simon Kemmerling <simon.kemmerling@brandung.de>
2014-03-21 14:13:30 +0100

    insert loadModule function

--- mod-simple-flexslider.js
+++ mod-simple-flexslider.js
@@ -2,26 +2,19 @@
  * mod flexslider
  * https://github.com/brandung/mod-simple-flexslider
  *
- * @type {{modFlexslider: modFlexslider}}
- * @dependencies {flexslider}
+ * @require {loadModule}
+ * @require {flexslider}
  */
 Brandung.Plugins.modFlexslider = function () {
-	if ($('.mod-simple-flexslider-wrapper').length !== 0) {
-		$.when(
-				$('<link>')
-					.appendTo($('head'))
-					.attr({
-						type: 'text/css',
-						rel: 'stylesheet'
-					})
-					.attr('href', Brandung.folderPath + '/libs/vendor/flexslider/flexslider.css'),
-				$.get(Brandung.folderPath + '/libs/vendor/flexslider/jquery.flexslider-min.js')
-			).then(function () {
-				$('.mod-simple-flexslider-wrapper .flexslider').flexslider({
-					animation: "slide"
-				});
-			});
-	}
+	$('.mod-simple-flexslider-wrapper').loadModule([
+		Brandung.folderPath + '/libs/vendor/flexslider/flexslider.css',
+		Brandung.folderPath + '/libs/vendor/flexslider/jquery.flexslider-min.js'
+	],
+	function () {
+		$('.mod-simple-flexslider-wrapper .flexslider').flexslider({
+			animation: "slide"
+		});
+	});
 };
 
 Brandung.Plugins.modFlexslider();
